1898ca227ae2f73d472a62009fe429f1e92be9a9
lemonjuiceexe <74901431+lemonjuiceexe@users.noreply.github.com>
2024-02-24 18:33:35 +0100

    Implement player secret and ready status toggling

--- chinese/src/app/components/lobby/lobby.component.ts
+++ chinese/src/app/components/lobby/lobby.component.ts
@@ -1,14 +1,16 @@
-import {Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {Color} from "../board/board/board.component";
 import {FormsModule} from "@angular/forms";
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
+import { LobbyService } from "../../services/lobby.service";
 
-interface Player{
+export interface Player{
   name: string;
-  ready: boolean;
+  secret: string;
+  isReady: boolean;
   color: Color;
 }
-interface Lobby{
+export interface Lobby{
   id: number;
   players: Player[];
   gameState: Object;
@@ -17,46 +19,61 @@ interface JoinLobbyResponse {
   lobby: Lobby;
   player: Player;
 }
+interface ToggleReadyResponse {
+  lobby: Lobby;
+}
 
 @Component({
   selector: 'app-lobby',
   standalone: true,
   imports: [
     FormsModule,
-    NgIf
+    NgIf,
+    NgForOf
   ],
   templateUrl: './lobby.component.html',
-  styleUrl: './lobby.component.css'
+  styleUrl: './lobby.component.css',
 })
 export class LobbyComponent {
   lobby: Lobby | null = null;
   player: Player = {
     name: "",
-    ready: false,
+    secret: "",
+    isReady: false,
     color: Color.Red
   };
 
-  constructor() { }
+  constructor(private lobbyService: LobbyService) { }
   joinLobby(e: Event) {
     e.preventDefault();
     e.stopPropagation();
-    console.log("Joining lobby !!!!!");
-    fetch(`http://127.0.0.1/chinese/server/join_lobby.php?playerName=${this.player.name}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "http://localhost",
-        "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Allow-Credentials",
-      },
-      credentials: "include"
-    })
+    this.lobbyService.joinLobby(this.player.name)
       .then(response => response.json())
       .then((response: JoinLobbyResponse) => {
         this.lobby = response.lobby;
         this.player = response.player;
+        localStorage.setItem("player", JSON.stringify(this.player));
+        localStorage.setItem("lobby", JSON.stringify(this.lobby));
 
         console.log(JSON.stringify(this.lobby));
         console.log("im " + this.player);
+      });
+      // .then(text => text.text())
+      // .then(text => console.log(text));
+  }
+  toggleReady($event: MouseEvent) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    if(!this.lobby)
+      return;
+    this.lobbyService.toggleReady(this.player, this.lobby)
+      .then(response => response.json())
+      .then((response) => {
+        this.lobby = response.lobby;
+        console.log(JSON.stringify(this.lobby));
+        localStorage.setItem("lobby", JSON.stringify(this.lobby));
+        this.player = this.lobby!.players.find(player => player.secret === this.player.secret)!;
+        localStorage.setItem("player", JSON.stringify(this.player));
       });
       // .then(text => text.text())
       // .then(text => console.log(text));
--- chinese/src/app/services/lobby.service.ts
+++ chinese/src/app/services/lobby.service.ts
@@ -1,19 +1,34 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Lobby, Player} from "../components/lobby/lobby.component";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LobbyService {
 
-  constructor() { }
+  constructor() {
+  }
   joinLobby(playerName: string) {
-    console.log("Joining lobby as " + playerName);
     return fetch(`http://127.0.0.1/chinese/server/join_lobby.php?playerName=${playerName}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "localhost"
+        "Access-Control-Allow-Origin": "http://localhost",
+        "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Allow-Credentials",
       }
+    });
+  }
+  toggleReady(player: Player, lobby: Lobby) {
+    const formData = new FormData();
+    formData.append("playerSecret", player.secret);
+    formData.append("lobbyId", lobby.id.toString());
+    return fetch(`http://127.0.0.1/chinese/server/toggle_ready.php`, {
+      method: "POST",
+      headers: {
+        "Access-Control-Allow-Origin": "http://localhost",
+        "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Allow-Credentials",
+      },
+      body: formData
     });
   }
 }
