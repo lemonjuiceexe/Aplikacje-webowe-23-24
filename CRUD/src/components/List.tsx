4133b88746637981f1e0d76bbd617aa67a47b044
lemonjuiceexe <74901431+lemonjuiceexe@users.noreply.github.com>
2024-03-11 13:46:54 +0100

    Setup daisyui

--- CRUD/src/components/List.tsx
+++ CRUD/src/components/List.tsx
@@ -14,7 +14,27 @@ export default function List() {
     return (
         <ul role="list" className="divide-y divide-gray-100">
             {movies.map((movie) => (
-               <p>{movie.title}</p>
+                <li key={movie.id} className="py-4 flex flex-row justify-evenly">
+                    <div className="ml-3">
+                        <p className="text-sm font-medium text-gray-900">
+                            {movie.title}
+                        </p>
+                        <p className="text-sm text-gray-500">
+                            {movie.year}
+                        </p>
+                    </div>
+                    <div>
+                        <p className="text-sm text-gray-900">
+                            {movie.length}
+                        </p>
+                    </div>
+                    <div>
+                        <p className="text-sm text-gray-900">
+                            &#9733;
+                            {movie.rating}
+                        </p>
+                    </div>
+                </li>
             ))}
         </ul>
     )
